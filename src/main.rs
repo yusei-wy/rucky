43933a4b88cf2d6115c3b357cf2f7e7b448cdb8e
yusei-wy <yusei.kasa@gmail.com>
2019-03-17 10:12:16 +0900

    Added repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate rucky;
+
+use std::io::{stdin, stdout, Write};
+
+use rucky::lexer::Lexer;
+use rucky::token::Token;
+
+const PROMPT: &str = ">> ";
+
+fn get_input() -> String {
+    let mut s = String::new();
+    stdin().read_line(&mut s).unwrap();
+    s.trim().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("{}", PROMPT);
+        let _ = stdout().flush();
+
+        let line: String = get_input();
+
+        if line == "exit" {
+            break;
+        }
+
+        let mut l = Lexer::new(&line);
+
+        loop {
+            let tok = l.next_token();
+            if tok == Token::EOF {
+                break;
+            }
+            println!("{:?}", tok);
+        }
+    }
 }
